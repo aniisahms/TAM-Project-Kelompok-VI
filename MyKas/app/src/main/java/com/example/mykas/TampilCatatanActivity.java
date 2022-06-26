62ae805ea8612d5c49881501b9ceb2106f16739a
aniisahms <83533036+aniisahms@users.noreply.github.com>
2022-06-26 20:33:42 +0700

    Update TampilCatatanActivity.java

--- MyKas/app/src/main/java/com/example/mykas/TampilCatatanActivity.java
+++ MyKas/app/src/main/java/com/example/mykas/TampilCatatanActivity.java
@@ -1,6 +1,14 @@
 package com.example.mykas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,9 +16,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class TampilCatatanActivity extends AppCompatActivity {
+public class TampilCatatanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private RecyclerView recyclerView;
     private ArrayList<Item> list;
+    private Button btnTambahCatatanBaru;
+    private Spinner rentangTampilCatSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,14 @@ public class TampilCatatanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampil_catatan);
 
+        // RENTANG TAMPIL CATATAN SPINNER
+        rentangTampilCatSpinner = findViewById(R.id.rentang_tampil_cat_spinner);
+        ArrayAdapter<CharSequence> adapterRentangTampil = ArrayAdapter.createFromResource(this, R.array.rentang_waktu_tampil_catatan, android.R.layout.simple_spinner_item);
+        adapterRentangTampil.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rentangTampilCatSpinner.setAdapter(adapterRentangTampil);
+        rentangTampilCatSpinner.setOnItemSelectedListener(this);
+
+        // RECYCLERVIEW
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
 
@@ -28,5 +46,26 @@ public class TampilCatatanActivity extends AppCompatActivity {
         AdapterData cardViewContactAdapter = new AdapterData(this);
         cardViewContactAdapter.setCatatanKeuanganList(list);
         recyclerView.setAdapter(cardViewContactAdapter);
+
+        // BUTTON TAMBAH CATATAN BARU
+        btnTambahCatatanBaru = findViewById(R.id.btn_tambah_catatan_baru);
+
+        btnTambahCatatanBaru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), TambahCatatanActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
