03c0bb544fe52616fb4cf93c4b0cffb90c6abf6e
aniisahms <83533036+aniisahms@users.noreply.github.com>
2022-06-26 20:39:19 +0700

    Update TambahCatatanActivity.java

--- MyKas/app/src/main/java/com/example/mykas/TambahCatatanActivity.java
+++ MyKas/app/src/main/java/com/example/mykas/TambahCatatanActivity.java
@@ -2,14 +2,131 @@ package com.example.mykas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 
-public class TambahCatatanActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class TambahCatatanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton, simpanCatatan;
+    private Spinner tipeSpinner, kategoriSpinner;
+    //String[] tipe, kategori;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().setTitle("Tambah Catatan Keuangan");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_catatan);
+
+        // Date picker
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
+
+        tipeSpinner = findViewById(R.id.tipe_spinner);
+        kategoriSpinner = findViewById(R.id.kategori_spinner);
+
+        // Tipe spinner
+        ArrayAdapter<CharSequence> adapterTipe = ArrayAdapter.createFromResource(this, R.array.tipe, android.R.layout.simple_spinner_item);
+        adapterTipe.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipeSpinner.setAdapter(adapterTipe);
+        tipeSpinner.setOnItemSelectedListener(this);
+
+        // Kategori spinner
+        ArrayAdapter<CharSequence> adapterKategori = ArrayAdapter.createFromResource(this, R.array.kategori, android.R.layout.simple_spinner_item);
+        adapterKategori.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        kategoriSpinner.setAdapter(adapterKategori);
+        kategoriSpinner.setOnItemSelectedListener(this);
+    }
+
+
+    // DATEPICKER
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void showDate(View view) {
+        datePickerDialog.show();
+    }
+
+
+    // TIPE & KATEGORI SPINNER
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
