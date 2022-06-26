d7010b647cc0708b44890d26ea240b05b7cd9955
aniisahms <83533036+aniisahms@users.noreply.github.com>
2022-06-26 21:17:54 +0700

    Update LaporanActivity.java

--- MyKas/app/src/main/java/com/example/mykas/LaporanActivity.java
+++ MyKas/app/src/main/java/com/example/mykas/LaporanActivity.java
@@ -3,13 +3,35 @@ package com.example.mykas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class LaporanActivity extends AppCompatActivity {
+public class LaporanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner rentangWaktuLaporanSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().setTitle("Laporan Keuangan");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laporan);
+
+        // RENTANG WAKTU LAPORAN SPINNER
+        rentangWaktuLaporanSpinner = findViewById(R.id.rentang_waktu_laporan_spinner);
+        ArrayAdapter<CharSequence> adapterRentangLaporan = ArrayAdapter.createFromResource(this, R.array.rentang_waktu_laporan, android.R.layout.simple_spinner_item);
+        adapterRentangLaporan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rentangWaktuLaporanSpinner.setAdapter(adapterRentangLaporan);
+        rentangWaktuLaporanSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
